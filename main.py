5a4da507bdeb966dce4a5beecad8c7e4dd9c439e
Ridu <49078437+redham5@users.noreply.github.com>
2022-12-20 21:38:12 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from pickle import TRUE
 import pyautogui as pag
 import random
 import time
-while TRUE:
+while True:
     x = random.randint(600,700)
     y = random.randint(200,600)
     pag.moveTo(x,y,0.5)
